b180fe0c9e8ae36c4892a36a6214ebdfe567cd88
KalleHyvo <anguskaani@gmail.com>
2024-04-17 16:02:22 +0300

    Lisäsin jcdb ajurin (ehkä?) Muokkasin module-info.java kansiota (requires java.sql)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module student.example.mokkivarausjarjestelmajava_ht {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens student.example.mokkivarausjarjestelmajava_ht to javafx.fxml;
     exports student.example.mokkivarausjarjestelmajava_ht;
--- src/main/java/student/example/mokkivarausjarjestelmajava_ht/Testing.java
+++ src/main/java/student/example/mokkivarausjarjestelmajava_ht/Testing.java
@@ -1,5 +1,6 @@
 package student.example.mokkivarausjarjestelmajava_ht;
 
+import java.sql.*;
 public class Testing {
     //Testaus committiin.
 }
