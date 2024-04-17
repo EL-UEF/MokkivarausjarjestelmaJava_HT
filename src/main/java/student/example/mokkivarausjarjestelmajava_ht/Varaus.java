ae3389c38a64189e0d8fb760cd7c03f3dfc24a15
NuuttiNiiranen <Nuutti23@gmail.com>
2024-04-17 15:46:18 +0300

    Muokattu varaus luokka siten, että se ei ole enää javaFX vaan pelkkä java

--- src/main/java/student/example/mokkivarausjarjestelmajava_ht/Varaus.java
+++ src/main/java/student/example/mokkivarausjarjestelmajava_ht/Varaus.java
@@ -3,9 +3,10 @@ package student.example.mokkivarausjarjestelmajava_ht;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-public class Varaus extends Application {
+public class Varaus {
     int varaus_id=0;
     int asiakas_id = 0;
     int mokki_id = 0;
@@ -13,14 +14,15 @@ public class Varaus extends Application {
     Date vahvistus_pvm = new Date();
     Date varattu_alkupvm = new Date();
     Date varattu_loppupvm = new Date();
+    ArrayList<Varaus> varausLista = new ArrayList<>();
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
+    public Varaus(int varaus_id, int asiakas_id, int mokki_id, Date varattu_pvm, Date vahvistus_pvm, Date varattu_alkupvm, Date varattu_loppupvm) {
+        this.varaus_id = varaus_id;
+        this.asiakas_id = asiakas_id;
+        this.mokki_id = mokki_id;
+        this.varattu_pvm = varattu_pvm;
+        this.vahvistus_pvm = vahvistus_pvm;
+        this.varattu_alkupvm = varattu_alkupvm;
+        this.varattu_loppupvm = varattu_loppupvm;
     }
 }
