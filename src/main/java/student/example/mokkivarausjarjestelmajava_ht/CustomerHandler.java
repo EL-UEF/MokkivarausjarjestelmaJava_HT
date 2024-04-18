17e0267a09cc2765e5f399f14b18e800f411d46c
NuuttiNiiranen <Nuutti23@gmail.com>
2024-04-18 16:12:55 +0300

    Jatkettu customerHandler luokan toiminnallisuutta, nyt sillä pystyy lisäämään uusia asiakkaita ja käyttämään koti-nappia

--- src/main/java/student/example/mokkivarausjarjestelmajava_ht/CustomerHandler.java
+++ src/main/java/student/example/mokkivarausjarjestelmajava_ht/CustomerHandler.java
@@ -3,10 +3,17 @@ package student.example.mokkivarausjarjestelmajava_ht;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -38,8 +45,16 @@ public class CustomerHandler extends Application {
             alueAsiakkaidenTiedoille.setText(
                     luettavaAsiakasLista.get(asiakasLista.getSelectionModel().getSelectedIndex()).toString());
         });
+        Button uusiAsiakas = new Button("Lisää uusi asiakas");
+        uusiAsiakas.setOnAction(e->{
+            asiakkaanLisaysMetodi(asiakasStage);
+        });
+        Button koti = main.kotiNappain(asiakasStage);
+        HBox paneeliAlaValikolle = new HBox(10);
+        paneeliAlaValikolle.getChildren().addAll(koti, uusiAsiakas);
         BPAsiakkaille.setLeft(asiakasLista);
         BPAsiakkaille.setCenter(alueAsiakkaidenTiedoille);
+        BPAsiakkaille.setBottom(paneeliAlaValikolle);
         Scene scene = new Scene(BPAsiakkaille);
         asiakasStage.setScene(scene);
         asiakasStage.setTitle("Asiakkaat");
@@ -51,6 +66,48 @@ public class CustomerHandler extends Application {
 
         olemassaolevatAsiakkaat.add(asiakas1);
         olemassaolevatAsiakkaat.add(asiakas2);
+    }
+    public void asiakkaanLisaysMetodi(Stage lisaysStage){
+        BorderPane paneeliAsiakkaidenLisääntymiselle = new BorderPane();
+        paneeliAsiakkaidenLisääntymiselle.setPrefSize(500, 500);
+        paneeliAsiakkaidenLisääntymiselle.setPadding(new Insets(10, 10, 10, 10));
+
+        VBox paneeliAsiakkaanTiedoille = new VBox(10);
+        Text uusiPostiNumero = new Text("Anna asiakkaan postinumero");
+        TextField postiNroTF = new TextField();
+        Text uusiEtunimi = new Text("Asiakkaan etunimi");
+        TextField etunimiTF = new TextField();
+        Text uusiSukunimi = new Text("Asiakkaan sukunimi");
+        TextField sukunimiTF = new TextField();
+        Text uusiOsoite = new Text("Asiakkaan lähiosoite");
+        TextField osoiteTF = new TextField();
+        Text uusiSposti = new Text("Asiakkaan sähköpostiosoite");
+        TextField spostiTF = new TextField();
+        Text uusiPuhNro = new Text("Asiakkaan puhelinnumero");
+        TextField puhelinnumeroTF = new TextField();
+        Button tallennusNappi = new Button("Tallenna");
+
+        tallennusNappi.setOnAction(e->{
+            int uusiID = olemassaolevatAsiakkaat.size()+1;
+            int uusiPostinro = Integer.parseInt(postiNroTF.getText());
+            String lisattavaEtunimi = etunimiTF.getText();
+            String lisattavaSukunimi = sukunimiTF.getText();
+            String lisattavaOsoite = osoiteTF.getText();
+            String lisattavaSposti = spostiTF.getText();
+            String lisattavaNro = puhelinnumeroTF.getText();
+            Asiakas uusiLisattavaAsiakas = new Asiakas(uusiID, uusiPostinro, lisattavaEtunimi, lisattavaSukunimi, lisattavaOsoite, lisattavaSposti, lisattavaNro);
+            olemassaolevatAsiakkaat.add(uusiLisattavaAsiakas);
+            System.out.println(uusiLisattavaAsiakas);
+        });
+        Button kotiNappi = main.kotiNappain(lisaysStage);
+        paneeliAsiakkaidenLisääntymiselle.setLeft(kotiNappi);
+        paneeliAsiakkaanTiedoille.getChildren().addAll(uusiPostiNumero, postiNroTF, uusiEtunimi, etunimiTF, uusiSukunimi, sukunimiTF, uusiOsoite, osoiteTF, uusiSposti, spostiTF, uusiPuhNro, puhelinnumeroTF, tallennusNappi);
+        paneeliAsiakkaanTiedoille.setAlignment(Pos.CENTER);
+        paneeliAsiakkaidenLisääntymiselle.setCenter(paneeliAsiakkaanTiedoille);
+        Scene scene = new Scene(paneeliAsiakkaidenLisääntymiselle);
+        lisaysStage.setScene(scene);
+        lisaysStage.setTitle("Uuden asiakkaan lisääminen");
+        lisaysStage.show();
     }
 
 
