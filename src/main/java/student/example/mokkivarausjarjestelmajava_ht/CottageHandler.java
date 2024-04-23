2a935e97c730d42fda8e3d716e9028373049eeb7
NuuttiNiiranen <Nuutti23@gmail.com>
2024-04-23 18:53:17 +0300

    CottageHandler toimii nyt kokonaan SQL tietokannan kanssa. Myös joitain JavaDoc kommentteja lisätty, teen javadocit loppuun joskus kun jaksan

--- src/main/java/student/example/mokkivarausjarjestelmajava_ht/CottageHandler.java
+++ src/main/java/student/example/mokkivarausjarjestelmajava_ht/CottageHandler.java
@@ -18,19 +18,45 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Luokka mökkeihin liittyvän käyttöliittymän grafiikalle ja logiikalle
+ * Luokka lisää, lukee, muokkaa ja poistaa tietoja suoraan SQL palvelimelta
+ */
 public class CottageHandler extends Application {
+    /**
+     * Luotu main instance jotta voidaan käyttää Mainin metodeja
+     */
     private Main main;
+    /**
+     * Luotu Mokki instance jotta voidaan käyttää Mokkin metodeja
+     */
     Mokki mokki;
 
+    /**
+     * Alustaja jota Main käyttää
+     * @param main Main instance
+     * @param mokki Mainissa luotu Mokki instance
+     */
     public CottageHandler(Main main, Mokki mokki) {
         this.main = main;
         this.mokki=mokki;
     }
+
+    /**
+     * Indeksi, joka kertoo mikä mökki on valittuna tällä hetkellä listViewissä.
+     * Alustettu olemaan -1 jotta ei voida vahingossa muokata minkään mökin tietoja valitsematta mökkiä
+     */
     int valittuIndeksi=-1;
 
-
-    ArrayList<Mokki> olemassaolevatMokit = new ArrayList<>();
-    protected void mokinLisaysMetodi(Stage mokkiStage){ //TOIMII SQL:SSÄ
+    /**
+     * Metodi, jolla voidaan lisätä mökkejä SQL tietokantaan
+     * Kaikki kentät on täytettävä jotta tämä metodi toimisi. Tämä tahallista, jotta saamme tarjottua asiakkaille täydelliset tiedot mökeistä
+     * @param mokkiStage Stage, jonka sisältö halutaan muuttaa mökkien lisäysvalikoksi
+     */
+    protected void mokinLisaysMetodi(Stage mokkiStage){
+        /**
+         * graafisia komponentteja ja niiden sijoittelua
+         */
         BorderPane BPMokinLisaamiselle = new BorderPane();
         VBox paneeliUudenMokinTiedoille = new VBox(10);
         Text annaAlue = new Text("Mille alueelle mökki kuuluu? (Alue ID, katuosoite ja postinumero)");
@@ -63,8 +89,10 @@ public class CottageHandler extends Application {
         paneeliUudenMokinTiedoille.getChildren().addAll(annaAlue, paneeliAlueelle, mokinNimi, nimiTF, hintaTeksti, hintaTF, kuvausTeksti, kuvausTF,
                 henkilomaaraTeksti, henkiloTF, paneeliMokinVarusteille, tallennusNappi);
         paneeliUudenMokinTiedoille.setAlignment(Pos.CENTER);
-
-        //toiminnallisuus
+        /**
+         * Toiminnallisuus tallennusnappiin.
+         * hakee tiedot kaikista textFieldeistä ja lisää mökin niiden perusteella
+         */
         tallennusNappi.setOnAction(e->{
             String mokinAlue = alueTF.getText();
             String mokinPostinumero = postinroTF.getText();
@@ -94,6 +122,9 @@ public class CottageHandler extends Application {
                 }
                 lisattavatVarusteet += "Hiustenkuivain";
             }
+            /**
+             * Käytetään main instanssissa olemassa olevaa connectionia SQL tietojen muokkaamiseen
+             */
             main.connect.insertData("mokki", "alue_id, postinro, mokkinimi, katuosoite, hinta, kuvaus, henkilomaara, varustelu",
                     (mokinAlue + ", " + mokinPostinumero + ", \"" + lisattavanMokinNimi + "\", \"" + lisattavaOsoite + "\", " + lisattavaHinta +
                             ", \"" + lisattavaKuvaus + "\", " + lisattavaHenkilomaara + ", \""+ lisattavatVarusteet + "\""));
@@ -103,11 +134,21 @@ public class CottageHandler extends Application {
         mokkiStage.setScene(lisaysScene);
         mokkiStage.show();
     }
-    protected void mokkiMetodi(Stage mokkiStage, ResultSet rs){ //TOIMII SQL:SSÄ
+
+    /**
+     * Luo valikon mökkienhallinnan päänäkymälle ja mökkien etsinnän tulokselle.
+     * Toimii SQL kanssa.
+     * @param mokkiStage stage, johon valikko halutaan tehdä
+     * @param rs resultset joka haetaan SQL tietokannasta, sisältää mökkien tiedot
+     */
+    protected void mokkiMetodi(Stage mokkiStage, ResultSet rs){
         BorderPane BPmokeille = new BorderPane();
         TextArea alueMokkienTiedoille = new TextArea();
         alueMokkienTiedoille.setText("Klikkaa mökkiä nähdäksesi sen tarkemmat tiedot :)");
         alueMokkienTiedoille.setEditable(false);
+        /**
+         * Logiikka mökkien indeksien näyttämiselle ListViewissä ja tietojen hakemiselle tietokannasta
+         */
         ArrayList<String> mokkiNimiLista = new ArrayList<>();
         try {
             while (rs.next())
@@ -129,7 +170,7 @@ public class CottageHandler extends Application {
         });
         Button poistoNappi = new Button("Poista valittu mökki");
         poistoNappi.setOnAction(e->{
-            mokinPoisto(valittuIndeksi);
+            mokinPoisto();
         });
         Button muokkausNappi = new Button("Muokkaa valittua mökkiä");
         muokkausNappi.setOnAction(e->{
@@ -137,7 +178,7 @@ public class CottageHandler extends Application {
         });
         Button etsintaNappi = new Button("Etsi mökkiä");
         etsintaNappi.setOnAction(e->{
-            uusiMokinEtsintaMetodi(mokkiStage);
+            mokinEtsintaMetodi(mokkiStage);
         });
         HBox paneeliAlaValikolle = new HBox(10);
         paneeliAlaValikolle.getChildren().addAll(kotiNappi, lisaysNappi, muokkausNappi, etsintaNappi, poistoNappi);
@@ -149,7 +190,7 @@ public class CottageHandler extends Application {
         mokkiStage.setScene(scene);
         mokkiStage.show();
     }
-    public void mokinPoisto(int poistettavaIndeksi){ //TOIMII SQL:SSÄ
+    public void mokinPoisto(){ //TOIMII SQL:SSÄ
         VBox varoitusPaneeli = new VBox(30);
         varoitusPaneeli.setPrefSize(300, 300);
         varoitusPaneeli.setPadding(new Insets(10, 10, 10, 10));
@@ -176,7 +217,7 @@ public class CottageHandler extends Application {
         popUpStage.setTitle("VAROITUS");
         popUpStage.show();
     }
-    protected void uusiMokinEtsintaMetodi(Stage etsintaStage){
+    protected void mokinEtsintaMetodi(Stage etsintaStage){ //TOIMII SQL:SSÄ
         BorderPane BPMokinEtsinnalle = new BorderPane();
         VBox paneeliEtsintaKriteereille = new VBox(10);
         paneeliEtsintaKriteereille.setAlignment(Pos.CENTER);
@@ -258,119 +299,10 @@ public class CottageHandler extends Application {
         etsintaStage.setScene(scene);
         etsintaStage.show();
     }
-    public void mokinEtsintaMetodi(Stage etsintaStage){ //MUOKATTAVA TOIMIMAAN SQL:SSÄ
-        BorderPane BPMokinEtsinnalle = new BorderPane();
-        VBox paneeliEtsintaKriteereille = new VBox(10);
-        paneeliEtsintaKriteereille.setAlignment(Pos.CENTER);
-        Text alkuHopina = new Text("Kirjoita haluamasi kriteerit alla oleviin kenttiin.\nVoit jättää kentän tyhjäksi jos et halua käyttää kyseistä kriteeriä");
-        Text nimiKriteeri = new Text("Mökin nimi");
-        TextField nimiTF = new TextField();
-        Text alueKriteeri = new Text("Alueen ID, jolla mökki sijaitsee");
-        TextField alueTF = new TextField();
-        Text osoiteKriteeri = new Text("Mökin katuosoite");
-        TextField osoiteTF = new TextField();
-        Text hinnat = new Text("Mökin minimi ja maksimi hinta");
-        HBox paneeliHinnoille = new HBox(10);
-        TextField minimiHinta = new TextField();
-        TextField maksimiHinta = new TextField();
-        Label minimiLabel = new Label("Min", minimiHinta);
-        minimiLabel.setContentDisplay(ContentDisplay.RIGHT);
-        Label maksimiLabel = new Label("Max", maksimiHinta);
-        maksimiLabel.setContentDisplay(ContentDisplay.RIGHT);
-        paneeliHinnoille.getChildren().addAll(minimiLabel, minimiHinta, maksimiLabel, maksimiHinta);
-        Text henkilomaaraKriteeri = new Text("Minimi henkilömäärä");
-        TextField henkilomaaraTF = new TextField();
-        Text varusteetKriteeri = new Text("Varusteet");
-        VBox paneeliCheckBoxeille = new VBox(10);
-        paneeliCheckBoxeille.setAlignment(Pos.CENTER);
-        CheckBox keittio = new CheckBox("Keittiö");
-        CheckBox sauna = new CheckBox("Sauna");
-        CheckBox latu = new CheckBox("Hiihtolatu lähellä");
-        CheckBox kuivain = new CheckBox("Hiustenkuivain");
-        paneeliCheckBoxeille.getChildren().addAll(keittio, sauna, latu, kuivain);
-        Button etsi = new Button("Etsi");
-        etsi.setOnAction(e->{
-            ArrayList<Mokki> nimiKay = new ArrayList<>();
-            ArrayList<Mokki> alueKay = new ArrayList<>();
-            ArrayList<Mokki> osoiteKay = new ArrayList<>();
-            ArrayList<Mokki> tarpeeksiKallis = new ArrayList<>();
-            ArrayList<Mokki> tarpeeksiHalpa = new ArrayList<>();
-            ArrayList<Mokki> tarpeeksiIso = new ArrayList<>();
-            ArrayList<Mokki> kaikkiKay = new ArrayList<>();
-            if (!nimiTF.getText().isEmpty()){
-                for (int i=0; i < olemassaolevatMokit.size(); i++){
-                    if (olemassaolevatMokit.get(i).mokkinimi.equalsIgnoreCase(nimiTF.getText())){
-                        nimiKay.add(olemassaolevatMokit.get(i));
-                    }
-                }
-            } else nimiKay.addAll(olemassaolevatMokit);
-            if (!alueTF.getText().isEmpty()){
-                for (int i=0; i < nimiKay.size(); i++){
-                    if (nimiKay.get(i).alue_id==Integer.parseInt(alueTF.getText())){
-                        alueKay.add(nimiKay.get(i));
-                    }
-                }
-            } else alueKay.addAll(nimiKay);
-            if (!osoiteTF.getText().isEmpty()){
-                for (int i=0; i < alueKay.size(); i++){
-                    if (alueKay.get(i).katuosoite.equalsIgnoreCase(osoiteTF.getText())){
-                        osoiteKay.add(alueKay.get(i));
-                    }
-                }
-            } else osoiteKay.addAll(alueKay);
-            if (!minimiHinta.getText().isEmpty()){
-                for (int i=0; i < osoiteKay.size(); i++){
-                    if (osoiteKay.get(i).hinta>=Double.parseDouble(minimiHinta.getText())){
-                        tarpeeksiKallis.add(osoiteKay.get(i));
-                    }
-                }
-            } else tarpeeksiKallis.addAll(osoiteKay);
-            if (!maksimiHinta.getText().isEmpty()){
-                for (int i=0; i < tarpeeksiKallis.size(); i++){
-                    if (tarpeeksiKallis.get(i).hinta<=Double.parseDouble(maksimiHinta.getText())){
-                        tarpeeksiHalpa.add(tarpeeksiKallis.get(i));
-                    }
-                }
-            } else tarpeeksiHalpa.addAll(tarpeeksiKallis);
-            if (!henkilomaaraTF.getText().isEmpty()) {
-                for (int i = 0; i < tarpeeksiHalpa.size(); i++){
-                    if (tarpeeksiHalpa.get(i).henkilomaara>=Integer.parseInt(henkilomaaraTF.getText())){
-                        tarpeeksiIso.add(tarpeeksiHalpa.get(i));
-                    }
-                }
-            } else tarpeeksiIso.addAll(tarpeeksiHalpa);
-            ArrayList<String> mokinVarusteet = new ArrayList<>();
-            if (keittio.isSelected())
-                mokinVarusteet.add("Keittiö");
-            if (sauna.isSelected())
-                mokinVarusteet.add("Sauna");
-            if (latu.isSelected())
-                mokinVarusteet.add("Hiihtolatu lähellä mökkiä");
-            if (kuivain.isSelected())
-                mokinVarusteet.add("Hiustenkuivain");
-            if (keittio.isSelected()||sauna.isSelected()||latu.isSelected()||kuivain.isSelected()){
-                for (int i = 0; i < tarpeeksiIso.size(); i++){
-                    if (tarpeeksiIso.get(i).varustelu.containsAll(mokinVarusteet)){
-                        kaikkiKay.add(tarpeeksiIso.get(i));
-                    }
-                }
-            } else kaikkiKay.addAll(tarpeeksiIso);
-            System.out.println(kaikkiKay);
-        });
-
-        Button kotiNappi = main.kotiNappain(etsintaStage);
-        paneeliEtsintaKriteereille.getChildren().addAll(alkuHopina, nimiKriteeri, nimiTF, alueKriteeri, alueTF, osoiteKriteeri, osoiteTF, hinnat, paneeliHinnoille, henkilomaaraKriteeri, henkilomaaraTF, varusteetKriteeri, paneeliCheckBoxeille, etsi);
-        BPMokinEtsinnalle.setCenter(paneeliEtsintaKriteereille);
-        BPMokinEtsinnalle.setLeft(kotiNappi);
-        Scene scene = new Scene(BPMokinEtsinnalle);
-        etsintaStage.setTitle("Mökin haku");
-        etsintaStage.setScene(scene);
-        etsintaStage.show();
-    }
-    public void mokinMuokkausMetodi(Stage muokkausStage){ //MUOKATTAVA TOIMIMAAN SQL:SSÄ
+    public void mokinMuokkausMetodi(Stage muokkausStage){ //TOIMII SQL:SSÄ
         BorderPane BPMokinMuokkaukselle = new BorderPane();
         VBox paneeliMuokattavilleTiedoille = new VBox(10);
-        Text muokattavaMokki = new Text("Muokattava mökki: id: " + olemassaolevatMokit.get(valittuIndeksi).mokki_id + " nimi: " + olemassaolevatMokit.get(valittuIndeksi).mokkinimi);
+        Text muokattavaMokki = new Text("MUOKATTAVA MÖKKI\n" + mokki.SQLToString(valittuIndeksi));
         Text alueMuokkausTeksti = new Text("Uusi alue id (numero)");
         TextField alueTF = new TextField();
         Text postinroTeksti = new Text("Uusi postinumero");
@@ -395,32 +327,36 @@ public class CottageHandler extends Application {
         paneeliCheckBoxeille.getChildren().addAll(keittio, sauna, latu, kuivain);
         Button tallennusNappi = new Button("Tallenna");
         tallennusNappi.setOnAction(e->{
+            ArrayList<String> kriteeriLista = new ArrayList<>();
             if (!alueTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).alue_id=Integer.parseInt(alueTF.getText());
+                main.connect.updateTable("mokki", "alue_id", alueTF.getText(), ("mokki_id = " + valittuIndeksi));
             if (!postinroTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).postinro=Integer.parseInt(postinroTF.getText());
+                main.connect.updateTable("mokki", "postinro", postinroTF.getText(), ("mokki_id = " + valittuIndeksi));
             if (!nimiTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).mokkinimi=nimiTF.getText();
+                main.connect.updateTable("mokki", "mokkinimi", ("\"" + nimiTF.getText()) + "\"", ("mokki_id = " + valittuIndeksi));
             if (!osoiteTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).katuosoite=osoiteTF.getText();
+                main.connect.updateTable("mokki", "katuosoite", ("\"" + osoiteTF.getText()) + "\"", ("mokki_id = " + valittuIndeksi));
             if (!hintaTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).hinta=Double.parseDouble(hintaTF.getText());
+                main.connect.updateTable("mokki", "hinta", hintaTF.getText(), ("mokki_id = " + valittuIndeksi));
             if (!kuvausTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).kuvaus=kuvausTF.getText();
+                main.connect.updateTable("mokki", "kuvaus", ("\"" + kuvausTF.getText()) + "\"", ("mokki_id = " + valittuIndeksi));
             if (!henkilomaaraTF.getText().isEmpty())
-                olemassaolevatMokit.get(valittuIndeksi).henkilomaara=Integer.parseInt(henkilomaaraTF.getText());
-            if (keittio.isSelected()||sauna.isSelected()||latu.isSelected()||kuivain.isSelected()){
-                olemassaolevatMokit.get(valittuIndeksi).varustelu.clear();
-                if (keittio.isSelected())
-                    olemassaolevatMokit.get(valittuIndeksi).varustelu.add("Keittiö");
-                if (sauna.isSelected())
-                    olemassaolevatMokit.get(valittuIndeksi).varustelu.add("Sauna");
-                if (latu.isSelected())
-                    olemassaolevatMokit.get(valittuIndeksi).varustelu.add("Hiihtolatu lähellä mökkiä");
-                if (kuivain.isSelected())
-                    olemassaolevatMokit.get(valittuIndeksi).varustelu.add("Hiustenkuivain");
+                main.connect.updateTable("mokki", "henkilomaara", henkilomaaraTF.getText(), ("mokki_id = " + valittuIndeksi));
+            if (keittio.isSelected()) {
+                kriteeriLista.add("Keittiö");
             }
-            System.out.println(olemassaolevatMokit.get(valittuIndeksi));
+            if (sauna.isSelected()) {
+                kriteeriLista.add("Sauna");
+            }
+            if (latu.isSelected()) {
+                kriteeriLista.add("Hiihtolatu lähellä mökkiä");
+            }
+            if (kuivain.isSelected()) {
+                kriteeriLista.add("Hiustenkuivain");
+            }
+            String kriteerit = ("\"" + String.join(", ", kriteeriLista) + "\"");
+            main.connect.updateTable("mokki", "varustelu", kriteerit, ("mokki_id = " + valittuIndeksi));
+            System.out.println("toimii ehkä?");
         });
         paneeliMuokattavilleTiedoille.getChildren().addAll(muokattavaMokki, alueMuokkausTeksti, alueTF, postinroTeksti,
                 postinroTF, nimiTeksti, nimiTF, osoiteTeksti, osoiteTF, hintaTeksti,
@@ -435,7 +371,6 @@ public class CottageHandler extends Application {
         muokkausStage.setScene(scene);
         muokkausStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
